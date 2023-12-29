f9f566845396317c1c9cb92be245e0549044ff87
Sayidabdullakhon <127374049+SadBaxodirov@users.noreply.github.com>
2023-12-29 23:57:15 +0500

    Update task_2.cpp

--- src/cpp/task_2.cpp
+++ src/cpp/task_2.cpp
@@ -1,9 +1,66 @@
-/*
- * Author:
- * Date:
- * Name:
- */
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <cmath>
 
+using namespace std;
+
+struct Edge2 {
+    int to;
+    int capacity;
+    int flow;
+};
 class Problem2 {
 public:
+    void addEdge(vector<vector<Edge2>>& graph, int from, int to, int capacity) {
+        graph[from].push_back({to, capacity, 0});
+    }
+    int edmondsKarp(vector<vector<Edge2>>& graph, int source, int sink) {
+        int totalFlow = 0;
+        while (true) {
+            queue<int> q;
+            q.push(source);
+            vector<int> pred(graph.size(), -1);
+            while (!q.empty() && pred[sink] == -1) {
+                int cur = q.front();
+                q.pop();
+                for (const Edge2& e : graph[cur]) {
+                    if (pred[e.to] == -1 && e.to != source && e.capacity > e.flow) {
+                        pred[e.to] = cur;
+                        q.push(e.to);
+                    }
+                }
+            }
+            if (pred[sink] == -1) {
+                break;
+            }
+            int pathFlow = INFINITY;
+            for (int u = sink; u != source; u = pred[u]) {
+                for (const Edge2& e : graph[pred[u]]) {
+                    if (e.to == u) {
+                        pathFlow = min(pathFlow, e.capacity - e.flow);
+                        break;
+                    }
+                }
+            }
+            for (int u = sink; u != source; u = pred[u]) {
+                for (Edge2& e : graph[pred[u]]) {
+                    if (e.to == u) {
+                        e.flow += pathFlow;
+                        break;
+                    }
+                }
+                for (Edge2& e : graph[u]) {
+                    if (e.to == pred[u]) {
+                        e.flow -= pathFlow;
+                        break;
+                    }
+                }
+            }
+            totalFlow += pathFlow;
+        }
+        return totalFlow;
+    }
 };
+
+
